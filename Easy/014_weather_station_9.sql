0bcdb4c5d62ada24cda34d8a857265a75afbbb78
Aishwarya <aishke883@gmail.com>
2025-08-18 01:18:47 +0530

    Create 014_weather_station_9.sql

---
+++ Easy/014_weather_station_9.sql
@@ -0,0 +1,2 @@
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.*/
+select distinct city from station where city not like 'A%' and city not like 'E%' and city not like 'I%' and city not like 'O%' and city not like 'U%';
