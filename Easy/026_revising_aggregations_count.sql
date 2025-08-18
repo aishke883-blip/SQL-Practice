c1bb2495e0915430fdf15be07e0ac5e407fa80e4
Aishwarya <aishke883@gmail.com>
2025-08-18 20:18:41 +0530

    Update 026_revising_aggregations_count.sql

--- Easy/026_revising_aggregations_count.sql
+++ Easy/026_revising_aggregations_count.sql
@@ -1,11 +1,2 @@
-/*Generate the following two result sets:
-
-Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
-Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
-
-There are a total of [occupation_count] [occupation]s.
-where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
-
-Note: There will be at least two entries in the table for each type of occupation.*/
-select name ||'(' || substr(occupation,1,1) || ')' from occupations order by name;
-select 'There are a total of ' || count(*) || ' ' || lower(occupation) ||'s.' from occupations group by occupation order by count(*), occupation asc; 
+/*Query a count of the number of cities in CITY having a Population larger than 100000.*/
+select count(name) from city where population>100000;
