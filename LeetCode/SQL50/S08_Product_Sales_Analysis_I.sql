553adcb79f8b719472a5d22d6b15975e9f30c79a
Aishwarya <aishke883@gmail.com>
2025-08-20 00:11:03 +0530

    Update S08_Product_Sales_Analysis_I.sql

--- LeetCode/SQL50/S08_Product_Sales_Analysis_I.sql
+++ LeetCode/SQL50/S08_Product_Sales_Analysis_I.sql
@@ -1,2 +1,4 @@
 /*Product Sales Analysis I*/
 select product_name, year, price from sales join product on sales.product_id=product.product_id;
+/*OR*/
+select p.product_name, s.year, s.price from sales s join product p on s.product_id=p.product_id;
